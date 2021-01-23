49efbcc85452d5f6b9612e47e899d3e3b8f9795e
zhangyi-zy <13718664930@163.com>
2021-01-23 22:55:39 +0800

    s

--- src/pro_git/DoList.java
+++ src/pro_git/DoList.java
@@ -4,5 +4,7 @@ public class DoList {
 
 	public static void main(String[] args) {
 
+		System.out.println(1212);
+
 	}
 }
--- src/pro_git/GetList.java
+++ src/pro_git/GetList.java
@@ -3,8 +3,12 @@ package pro_git;
 public class GetList {
 
 
+
+
 	public static void main(String[] args) {
-		
+
+         System.out.println(1212);
+
 
 	}
 }
