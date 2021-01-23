a366b3857c841073b85116db17014ba1c4a870d1
zhangyi-zy <13718664930@163.com>
2021-01-24 00:25:09 +0800

    f

--- src/pro_git/DoList.java
+++ src/pro_git/DoList.java
@@ -3,7 +3,8 @@ package pro_git;
 public class DoList {
 
 	public static void main(String[] args) {
-
+		System.out.println("-fz-");
+		System.out.println("-fz-");
 		System.out.println(1212);
 
 	}
--- src/pro_git/GetList.java
+++ src/pro_git/GetList.java
@@ -3,7 +3,8 @@ package pro_git;
 public class GetList {
 
 	public static void main(String[] args) {
-
+		System.out.println("-fz-");
+		System.out.println("-fz-");
          System.out.println(1212);
 
 	}
