36e52bee16436bc0fedc7be4e789085c5695884f
zhangyi-zy <13718664930@163.com>
2021-01-23 23:56:59 +0800

    f

--- src/pro_git/DoList.java
+++ src/pro_git/DoList.java
@@ -4,5 +4,6 @@ public class DoList {
 
 	public static void main(String[] args) {
 		System.out.println("-分支-");
+		System.out.println("-分支-");
 	}
 }
--- src/pro_git/GetList.java
+++ src/pro_git/GetList.java
@@ -4,5 +4,6 @@ public class GetList {
 
 	public static void main(String[] args) {
 		System.out.println("-分支-");
+		System.out.println("-分支-");
 	}
 }
