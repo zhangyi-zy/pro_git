dfbb272d965dc0c88b4bc7919e425e4ada938751
zhangyi-zy <13718664930@163.com>
2021-02-02 23:41:49 +0800

    s

--- src/pro_git/GetList.java
+++ src/pro_git/GetList.java
@@ -2,6 +2,7 @@ package pro_git;
 
 public class GetList {
 
+
 	public static void main(String[] args) {
 		System.out.println("-fz-");
 		System.out.println("-fz-");
@@ -9,6 +10,5 @@ public class GetList {
 		System.out.println("-分支-");
 		System.out.println("-分支-");
 		System.out.println("-分支-");
-
 	}
 }
