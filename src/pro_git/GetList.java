78b8cefd7dc217587692215afc106506a456f9e9
zhangyi820 <zhangyi820@ZB-HQTD963.360buyAD.local>
2021-02-03 10:39:50 +0800

    s

--- src/pro_git/GetList.java
+++ src/pro_git/GetList.java
@@ -4,7 +4,5 @@ public class GetList {
 
 	public static void main(String[] args) {
 		 System.out.println(1);	
-		 System.out.println(2);
-
 	}
 }
--- src/pro_git/WhatList.java
+++ src/pro_git/WhatList.java
@@ -2,4 +2,8 @@ package pro_git;
 
 public class WhatList {
 
+	public static void main(String[] args) {
+		System.out.println(1);
+	}
+	
 }
