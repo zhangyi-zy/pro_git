34cdd8aed51f07239e6f45ccc25358eb4c89c6b6
zhangyi820 <zhangyi820@ZB-HQTD963.360buyAD.local>
2021-02-03 11:46:22 +0800

    s7

--- src/pro_git/DoList.java
+++ src/pro_git/DoList.java
@@ -3,6 +3,6 @@ package pro_git;
 public class DoList {
 
 	public static void main(String[] args) {
-	    System.out.println(6);
+	    System.out.println(5);
 	}
 }
--- src/pro_git/WhatList.java
+++ src/pro_git/WhatList.java
@@ -3,7 +3,7 @@ package pro_git;
 public class WhatList {
 
 	public static void main(String[] args) {
-		System.out.println(9);
+		System.out.println(7);
 	}
 	
 }
